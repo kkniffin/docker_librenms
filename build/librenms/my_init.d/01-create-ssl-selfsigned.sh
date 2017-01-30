7d845fbb61501ed6af0c4b0dd74f211bbff3ce90
root <root@netmgmt610wh01.polk.edu>
2017-01-30 21:26:20 +0000

    changes

---
+++ build/librenms/my_init.d/01-create-ssl-selfsigned.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create SSL Self Signed Certificate
+if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ] || [ ! -f /etc/ssl/private/nginx-selfsigned.key ]; then
+	openssl req -x509 -nodes -days 365 -newkey rsa:2048 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=www.example.com" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+	openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+fi
